907079805457e3f8629c4a555ab5595bb1fd7044
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-02-15 17:59:50 -0600

    right side nav bar added

--- eshop/src/components/Navbar.jsx
+++ eshop/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IoMdSearch } from 'react-icons/io';
 
 const MenuLinks = [
   {
@@ -28,8 +29,8 @@ const Navbar = () => {
       <div className='py-4'>
         <div className='container'>
             {/* Logo and Links section */}
-                <div>
-                    <a 
+                <div className='flex items-center gap-4'>
+                  <a 
                       href="#" className='text-primary font-semibold 
                       tracking-widest text-2xl uppercase 
                       sm:text-3xl'
@@ -37,25 +38,33 @@ const Navbar = () => {
                         ESHOP
                     </a>
                     {/* Menu Items */}
-                    <div>
-                      <ul> 
-                        {
-                          MenuLinks.map((data, index) => (
-                            <li>
-                              <a href='#'>{data.name}</a>
-                            </li>
-                          ))
-                        }
-                      </ul>
-                    </div>
+                  <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-4'> 
+                      {
+                        MenuLinks.map((data, index) => (
+                          <li key={index}>
+                          <
+                            a href={data.link}
+                            className='inline-block px-4 font-semibold text-gray-500
+                            hover:text-black dark:hover:text-white duration-200'
+                            >{data.name}</a>
+                        </li>
+                        ))
+                      }
+                    </ul>
+                  </div>
                 </div>
             {/* Navbar Right section */}
-                <div>
-                  <div>
-                    <div>
-
-                    </div>
+                <div className='flex justify-between items-center gap-4'>
+                  {/* Search Bar Section */}
+                  <div className='relative group hidden sm:block'>
+                      <input type='text' placeholder='search' className='search-bar'/>
+                      <IoMdSearch 
+                        className='text-xl text=gray-600 dark:text-gray-400'/>
                   </div>
+                    {/* Dark Mode Section */}
+                    <div>
+                    </div>
                 </div>
 
         </div>

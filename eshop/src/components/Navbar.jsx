228b86448dd48f055ba48d4ea74e5604b8a73f80
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-02-15 18:44:25 -0600

    search bar transition and dark mode styles

--- eshop/src/components/Navbar.jsx
+++ eshop/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z40'>
       <div className='py-4'>
-        <div className='container'>
+        <div className='container flex justify-between items-center'>
             {/* Logo and Links section */}
                 <div className='flex items-center gap-4'>
                   <a 

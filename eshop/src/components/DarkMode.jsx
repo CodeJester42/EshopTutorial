4f5b55a3a3dc6279abbc3b3beba580b7bdfd2f23
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-02-15 20:29:40 -0600

    Made light/dark mode functional with onclick/useFunction/useEffect/useState/setTheme

---
+++ eshop/src/components/DarkMode.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import LightButton from '../images/light-mode-button.png';
+import DarkButton from '../images/dark-mode-button.png';
+
+const DarkMode = () => {
+    const [theme, setTheme] = React.useState(localStorage.getItem('theme') ? 
+        localStorage.getItem('theme') : 'light');
+
+    const element = document.documentElement;
+    console.log(element);
+
+    React.useEffect(() => {
+        localStorage.setItem('theme', theme);
+        if (theme === 'dark') {
+            element.classList.add('dark');
+        } else {
+            element.classList.remove('dark');
+        }
+    })
+
+  return (
+    <div className='relative'>
+        <img 
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            src={LightButton} 
+            alt='light mode button'
+            className={`w-12 cursor-pointer absolute right-0 z-10
+            ${theme === 'dark' ? 'opacity-0' : 'opacity-100'}`} 
+        />
+        <img 
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            src={DarkButton} 
+            alt='dark mode button'
+            className={`w-12 cursor-pointer
+             `} 
+        />
+    </div>
+  )
+}
+
+export default DarkMode
--- eshop/src/components/Navbar.jsx
+++ eshop/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IoMdSearch } from 'react-icons/io';
 import { FaCartShopping } from 'react-icons/fa6';
+import DarkMode from './DarkMode.jsx';
 
 const MenuLinks = [
   {
@@ -69,12 +70,13 @@ const Navbar = () => {
                     <button className='relative p-3'>
                       <FaCartShopping className='text-xl text-gray-600 
                       dark:text-gray-400'/>
-                      <div className='w-4 h-4 bg-red-500 text-white rounded-full
+                      <div className='w-4 h-4 bg-red-500 text-white
                       rounded-full absolute top-0 right-0 flex items-center 
                       justify-center text-xs'>4</div>
                     </button>
                     {/* Dark Mode Section */}
                     <div>
+                      <DarkMode />
                     </div>
                 </div>
 
